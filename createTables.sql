de9fab7ecd0390f047127c6ce38b057be34c1c77
kazım <kazimnergiz123@gmail.com>
2023-03-27 01:20:42 +0300

    create tablo yapısı eklendi

--- createTables.sql
+++ createTables.sql
@@ -1,63 +1,99 @@
 -- Tasarımını yaptığınız tabloları oluşturan sql ifadeleri buraya yazınız.
 -- veri tiplerine, nullable olma durumuna, default değerine ve tablolar arası foreign key kullanımına dikkat.
-CREATE TABLE `urunler`(
-    `urun_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `urun_adi` TEXT NOT NULL,
-    `urun_detaylari` TEXT NOT NULL,
-    `urun_fiyati` DOUBLE NOT NULL,
-    `siparis_id` BIGINT NOT NULL,
-    PRIMARY KEY (`urun_id`)
+CREATE TABLE `urunler` (
+  `urun_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `urun_adi` varchar(255) NOT NULL,
+  `aciklama` text NOT NULL,
+  `fiyat` decimal(10,2) NOT NULL,
+  `stok` int NOT NULL,
+  `resim` varchar(255) DEFAULT NULL,
 );
 
-CREATE TABLE `kategoriler`(
-    `kategori_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `kategori_adi` TEXT NOT NULL,
-    PRIMARY KEY (`kategori_id`)
+CREATE TABLE `kullanicilar` (
+  `kullanici_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `ad` varchar(255) NOT NULL,
+  `soyad` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `sifre` varchar(255) NOT NULL,
+  `telefon` varchar(20) DEFAULT NULL,
+) ;
+
+CREATE TABLE `siparisler` (
+  `siparis_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `kullanici_id` int NOT NULL,
+  `urun_id` int NOT NULL,
+  `adet` int NOT NULL,
+  `toplam_tutar` decimal(10,2) NOT NULL,
+  `tarih` datetime default NOT NULL,
+ FOREIGN KEY (`kullanici_id`) REFERENCES `kullanicilar` (`kullanici_id`) ON DELETE CASCADE,
+ FOREIGN KEY (`urun_id`) REFERENCES `urunler` (`urun_id`) ON DELETE CASCADE
 );
 
-CREATE TABLE `urun_kategori`(
-    `urun_id` BIGINT NOT NULL,
-    `kategori_id` BIGINT NOT NULL
-);
-CREATE TABLE `kullanıcı`(
-    `kullanici_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `kullanici_adi` TEXT NOT NULL,
-    `kullanici_soyadi` TEXT NOT NULL,
-    `kullanici_eposta` TEXT NOT NULL,
-    `kullanici_telefon` INT NOT NULL,
-    `kullanici_adres` TEXT NOT NULL,
-    PRIMARY KEY (`kullanici_id`)
+CREATE TABLE `adresler` (
+  `adres_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `kullanici_id` int NOT NULL,
+  `adres_basligi` varchar(255) NOT NULL,
+  `adres` text NOT NULL,
+  `sehir` varchar(255) NOT NULL,
+  `posta_kodu` varchar(10) NOT NULL,
+  FOREIGN KEY (`kullanici_id`) REFERENCES `kullanicilar` (`kullanici_id`) ON DELETE CASCADE
 );
 
-CREATE TABLE `siparisler`(
-    `siparis_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `kullanici_id` BIGINT NOT NULL,
-    `siparis_tarihi` DATETIME NOT NULL,
-    `teslimat_tarihi` DATETIME NOT NULL,
-    `durum` TEXT NOT NULL,
-    PRIMARY KEY (`siparis_id`)
+CREATE TABLE `sepet` (
+  `sepet_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `kullanici_id` int NOT NULL,
+  `urun_id` int NOT NULL,
+  `adet` int NOT NULL,
+  FOREIGN KEY (`kullanici_id`) REFERENCES `kullanicilar` (`kullanici_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`urun_id`) REFERENCES `urunler` (`urun_id`) ON DELETE CASCADE
 );
 
-CREATE TABLE `magaza_urunler`(
-    `magaza_id` BIGINT NOT NULL,
-    `urun_id` BIGINT NOT NULL
-);
-CREATE TABLE `magazalar`(
-    `magaza_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `magaza_adi` TEXT NOT NULL,
-    `magaza_adres` TEXT NOT NULL,
-    PRIMARY KEY (`magaza_id`)
+CREATE TABLE `iade` (
+  `iade_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `siparis_id` int NOT NULL,
+  `urun_id` int NOT NULL,
+  `kullanici_id` int NOT NULL,
+  `aciklama` text,
+  `islem_tarihi` datetime default NOT NULL,
+  `durum` enum('Beklemede','İşlemde','Tamamlandı') NOT NULL,
+  FOREIGN KEY (`siparis_id`) REFERENCES `siparisler` (`siparis_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`urun_id`) REFERENCES `urunler` (`urun_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`kullanici_id`) REFERENCES `kullanicilar` (`kullanici_id`) ON DELETE CASCADE
 );
 
-ALTER TABLE
-    `urunler` ADD CONSTRAINT `urunler_siparis_id_foreign` FOREIGN KEY(`siparis_id`) REFERENCES `siparisler`(`siparis_id`);
-ALTER TABLE
-    `magaza_urunler` ADD CONSTRAINT `magaza_urunler_magaza_id_foreign` FOREIGN KEY(`magaza_id`) REFERENCES `magazalar`(`magaza_id`);
-ALTER TABLE
-    `urun_kategori` ADD CONSTRAINT `urun_kategori_urun_id_foreign` FOREIGN KEY(`urun_id`) REFERENCES `urunler`(`urun_id`);
-ALTER TABLE
-    `urun_kategori` ADD CONSTRAINT `urun_kategori_kategori_id_foreign` FOREIGN KEY(`kategori_id`) REFERENCES `kategoriler`(`kategori_id`);
-ALTER TABLE
-    `magaza_urunler` ADD CONSTRAINT `magaza_urunler_urun_id_foreign` FOREIGN KEY(`urun_id`) REFERENCES `urunler`(`urun_id`);
-ALTER TABLE
-    `siparisler` ADD CONSTRAINT `siparisler_kullanici_id_foreign` FOREIGN KEY(`kullanici_id`) REFERENCES `kullanıcı`(`kullanici_id`);
+CREATE TABLE `inceleme` (
+  `inceleme_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `kullanici_id` int NOT NULL,
+  `urun_id` int NOT NULL,
+  `yorum` text NOT NULL,
+  `puan` tinyint(4) NOT NULL,
+  `tarih` datetime default NOT NULL,
+  FOREIGN KEY (`kullanici_id`) REFERENCES `kullanicilar` (`kullanici_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`urun_id`) REFERENCES `urunler` (`urun_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `odeme` (
+  `odeme_id` int NOT NULL AUTO_INCREMENT  PRIMARY KEY,
+  `kullanici_id` int NOT NULL,
+  `siparis_id` int NOT NULL,
+  `odeme_turu` enum('Kredi Kartı','Nakit') NOT NULL,
+  `tutar` decimal(10,2) NOT NULL,
+  `odeme_tarihi` datetime default NOT NULL,
+  FOREIGN KEY (`kullanici_id`) REFERENCES `kullanicilar` (`kullanici_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`siparis_id`) REFERENCES `siparisler` (`siparis_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `kargo` (
+  `kargo_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `siparis_id` int NOT NULL,
+  `kargo_sirketi` varchar(255) NOT NULL,
+  `kargo_numarasi` varchar(255) NOT NULL,
+  `tarih` datetime default NOT NULL,
+  FOREIGN KEY (`siparis_id`) REFERENCES `siparisler` (`siparis_id`) ON DELETE CASCADE
+);
+
+
+
+
+
+
